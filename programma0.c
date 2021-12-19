fed494789099b53c6bf598986740276d56345ddc
davideciancaofficial <davidecianca@gmail.com>
2021-12-19 14:14:17 +0100

    Create programma0.c

---
+++ programma0.c
@@ -0,0 +1,10 @@
+// First program in C. Print a message on the console.
+
+#include <stdio.h> // Command used to import the standard input/output header file.
+
+// Each function in C returns a value as an output and takes a value as an input.
+// In this case, main returns an integer and takes nothing as input.
+int main(void)
+{
+	printf("First program in C!\n"); //The printf function prints a string to the console. The escape sequence \n is used for carriage return.
+}
