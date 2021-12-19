86776cad91d0fff6d20fc13d3f4c44c80b710d4a
davideciancaofficial <davidecianca@gmail.com>
2021-12-19 14:30:11 +0100

    Sum of two integers

---
+++ TwoIntegersSum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int integer1;
+	int integer2;	
+	int sum;
+
+	printf("Insert the first integer: \n");
+	scanf("%d", &integer1);
+	printf("Insert the second integer: \n");
+	scanf("%d", &integer2);
+
+	sum = integer1 + integer2;
+	
+	printf("The sum is: %d\n", sum);
+}
